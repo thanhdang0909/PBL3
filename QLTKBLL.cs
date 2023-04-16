31a1916befd317538c61eb451795702b63d4bc1b
NhungLe <hongnhung16052003@gmail.com>
2023-04-16 13:15:19 +0700

    Add project

---
+++ QLTKBLL.cs
@@ -0,0 +1,43 @@
+﻿using DoAnPBL3.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DoAnPBL3.BLL
+{
+    public class QLTKBLL
+    {
+        public static QLTKBLL Instance = new QLTKBLL();
+        private string _tenDangNhap;
+        public string tenDangNhap { get => _tenDangNhap; set => _tenDangNhap = value; }
+
+        public void DangNhap(string tendangnhap, string matkhau, bool vaitro)
+        {
+            QLNH_DB db = new QLNH_DB();
+            var user = db.TAIKHOANs.Where(p => p.tenDangNhap == tendangnhap && p.matKhau == matkhau && p.vaiTro == vaitro).FirstOrDefault();
+            if (user == null)
+            {
+                MessageBox.Show("Đăng nhập thất bại! \nSai tài khoản đăng nhập!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                switch (vaitro)
+                {
+                    case false:
+                        new UINhaHangNV().ShowDialog();
+                        break;
+                    case true:
+                        new UINhahangAdmin().ShowDialog();
+                        break;
+                }
+
+            }
+
+        }
+    }
+}
